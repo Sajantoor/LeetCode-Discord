e7416b715234dd436c7bf26a416453418046791c
Sajan Toor <sajantoor98@gmail.com>
2022-01-01 17:42:29 -0800

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LeetCode Discord Bot
+
+Discord bot for LeetCode.
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import Discord from "discord.js";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+import Leetcode from "leetcode-api";
+import Problem from "leetcode-api/dist/lib/problem";
+
+dotenv.config();
+
+const main = async () => {
+    console.log("Hello World!");
+};
+
+main();

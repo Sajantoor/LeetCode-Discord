755cad752765389bd8e19617bf462f9511212e0a
Sajan Toor <sajantoor98@gmail.com>
2022-07-03 13:56:02 -0700

    Change default prefix to "." from "!"

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -3,5 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
-export const PREFIX = '!';
+export const PREFIX = '.';
 export const DISCORD_CODE_FORMATTER = "```";
